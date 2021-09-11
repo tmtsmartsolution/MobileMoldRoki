068f8f951fc476eb0a6dc92d84cbeb190bdd3f46
Thanakrit Jinakhan <thanakritv8@gmail.com>
2021-09-11 16:01:46 +0700

    change to tmt

--- src/app/services/state.service.ts
+++ src/app/services/state.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StateService {
-  //API_URL = 'https://1hzdevelop.thddns.net:2727/MoldLayout/api/';
-  API_URL = 'https://192.168.100.199:8080/MoldLayout/api/';
+  API_URL = 'https://virtualstore.tmt-digital.com/api/';
   constructor() { }
 }
